8c11891dfb903d8044609d7f5775de183a936e08
Anto <mrapoirotmarple@gmail.com>
2023-09-04 21:19:41 +0200

    Added same plots for R files

--- 3. Data Science Methodology/Data Science Methodology notebook-R.r
+++ 3. Data Science Methodology/Data Science Methodology notebook-R.r
@@ -185,3 +185,126 @@ for(plug_type in plug_types_needed){
     plug_type_image <- load.image(plug_type_icons[plug_type])
     plot(plug_type_image)
 }
+
+# We import ggplot2 so we can plot charts
+library(ggplot2)
+
+get_bar_chart_salaries_technology_in_country_by_experience_level <- function(programming_language, country){
+    # Displays a bar chart plot where we compare the salaries of a developer in a given technology in a given country by experience level
+    # programming_language: Programming language of the developers whose salary we will compare
+    # country: Country of the developers whose salary we will compare
+    
+    # We filter the dataset that contains the salary of developers to get average salaries in our country and programming language
+    df_salaries_country_technology <- salaries_dollar_dataframe[
+        (salaries_dollar_dataframe["Country"] == choosen_country & salaries_dollar_dataframe["Language"] == desired_programming_language),]
+    
+    # We keep only the columns with the salaries
+    df_salaries_country_technology <- df_salaries_country_technology[c("Entry", "Average", "Senior")]
+    
+    # By default, all bars are orange
+    bar_colors <- c('orange', 'orange', 'orange')
+    
+    # The column that represents the experience level we recommended previously will become blue
+    if(selected_experience_level == "Entry"){
+        bar_colors[1] <- "blue" 
+    }else if(selected_experience_level == "Average"){
+        bar_colors[2] <- "blue" 
+    }else{
+        bar_colors[3] <- "blue" 
+    }
+    
+    barplot(c(df_salaries_country_technology$Entry, df_salaries_country_technology$Average, df_salaries_country_technology$Senior), 
+            col = bar_colors, names.arg = names(df_salaries_country_technology))
+    
+}
+
+IRdisplay::display(paste0("- Salaries for ", desired_programming_language, " developers in ", choosen_country, " by experience level "))
+
+get_bar_chart_salaries_technology_in_country_by_experience_level(desired_programming_language, choosen_country)
+
+get_bar_chart_salaries_developer_country_experience_level_by_technology <- function(experience_level, country){
+    # Displays a bar chart plot where we compare the salaries of a developer with a given experience in a given country by technology level
+    # experience_level: Experience level of the developers
+    # country: Country of the developers
+    
+    # To start, we filter the dataframe with the average salaries to get only the ones for the programming language and experience level passed
+    # as parameters
+    df_salaries_technology_experience <- salaries_dollar_dataframe[
+            (salaries_dollar_dataframe["Country"] == country), c("Language", experience_level)]
+    
+    # We drop all row that have value NA
+    df_salaries_technology_experience <- na.omit(df_salaries_technology_experience)
+
+    # All columns will be orange, except the one suggested by us will be blue
+    bar_colors <- c()
+    for(language in df_salaries_technology_experience$Language){
+        if(language == desired_programming_language){
+            bar_colors <- append(bar_colors, 'blue')
+        }else{
+            bar_colors <- append(bar_colors, 'orange')
+        }
+    }
+    
+    # We set up the bar chart
+    barplot(df_salaries_technology_experience$Senior, 
+            col = bar_colors, names.arg = df_salaries_technology_experience$Language)
+    
+}
+
+IRdisplay::display(paste0("- Salaries for developers with experience level ", selected_experience_level, " in ", choosen_country))
+
+get_bar_chart_salaries_developer_country_experience_level_by_technology(selected_experience_level, choosen_country)
+
+# We import the libraries need to execute this function
+library(rjson)
+library(stringr)
+library(plotly)
+
+display_map_salaries_technology_experience_level_by_country <- function(programming_language, experience_level){
+    # Displays coloured map with the different average salaries of developers of a given programming language with a given experience
+    # level by country
+    # programming_language: Programming language used by the developers
+    # experience_level: Experience level of developers
+    
+    # To start, we filter the dataframe with the average salaries to get only the ones for the programming language and experience level passed
+    # as parameters
+    df_salaries_technology_experience <- salaries_dollar_dataframe[
+            (salaries_dollar_dataframe["Language"] == programming_language), c("Country", experience_level)]
+    
+    # We define a vector with keys to correct the names of the countries to match the ids in the json we will use to display countries in the map
+    countries_correction <- c("US" = "USA", "India" = "IND", "Germany" = "DEU", "UK" = "GBR", "France" = "FRA")
+    
+    # We correct the names of the countries in the dataframe to match the ids in the geo json
+    for(v_key in names(countries_correction)){
+        df_salaries_technology_experience$Country <- str_replace(df_salaries_technology_experience$Country, v_key, countries_correction[v_key])
+    }
+    
+    # We get the geo json to display countries in a map
+    url = "https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json"
+    countries <- rjson::fromJSON(file=url)
+    
+    # We set up the map
+    fig <- plot_ly() 
+    fig <- fig %>% add_trace(
+        type="choroplethmapbox",
+        geojson=countries,
+        locations=df_salaries_technology_experience$Country,
+        z=df_salaries_technology_experience[, selected_experience_level],
+        colorscale="Viridis",
+        zmin=min(df_salaries_technology_experience[, selected_experience_level], na.rm = TRUE),
+        zmax=max(df_salaries_technology_experience[, selected_experience_level], na.rm = TRUE),
+        marker=list(line=list(width=0),opacity=0.5))
+    fig <- fig %>% plotly::layout(
+        mapbox=list(
+            style="carto-positron",
+            zoom =1,
+            center=list(lon=0, lat=30))
+    )
+    fig
+}
+
+IRdisplay::display(paste0("Salaries for developers with experience level ", selected_experience_level, " in ", desired_programming_language))
+
+display_map_salaries_technology_experience_level_by_country(desired_programming_language, selected_experience_level)
+
+
